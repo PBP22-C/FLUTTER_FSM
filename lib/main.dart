b0b39ed121676c0390ccd5d27692a4f4a517b17a
liemroym <liemroymarcelino@gmail.com>
2022-11-19 23:37:53 +0700

    Added home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/pages/add_gedung_form.dart';
+import 'package:my_app/pages/home.dart';
 
 void main() => runApp(const AppFSM());
 
-class AppFSM extends StatelessWidget {
+class AppFSM extends StatefulWidget {
   const AppFSM({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    const appTitle = 'Flutter Form Demo';
-    return MaterialApp(title: appTitle, home: Home());
-  }
+  State<AppFSM> createState() => _AppFSMState();
 }
 
-class Home extends StatefulWidget {
-  const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
+class _AppFSMState extends State<AppFSM> {
   int _selectedIndex = 0;
 
-  final TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  final List<Widget> _widgetOptions = [
+  // final TextStyle optionStyle =
+  //     TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  final List<Widget> _pages = [
+    const Home(),
     const AddGedungForm(),
-    const Scaffold(body: Center(child: Text("Halaman")))
   ];
+
   @override
   Widget build(BuildContext context) {
-    const appTitle = 'Flutter Form Demo';
+    const appTitle = 'App FSM Flutter';
 
     void onTap(int index) {
-      print(index.toString());
       setState(() {
         _selectedIndex = index;
       });
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(appTitle),
-      ),
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          const BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          const BottomNavigationBarItem(icon: Icon(Icons.add), label: "Add")
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.green,
-        onTap: onTap,
-      ),
-    );
+    return (MaterialApp(
+        title: appTitle,
+        home: (Scaffold(
+          appBar: AppBar(
+            title: const Text(appTitle),
+          ),
+          body: _pages.elementAt(_selectedIndex),
+          bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(icon: Icon(Icons.add), label: "Form")
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.green,
+            onTap: onTap,
+          ),
+        ))));
   }
 }
